50f4c69f452a5832e78d54bb77d1796d83c43732
Andy Bulka <abulka@gmail.com>
2020-04-25 17:46:04 +1000

    initial definition of todoitem entity and rendering to console

--- js/app.js
+++ js/app.js
@@ -3,4 +3,37 @@
 
 	// Your starting point. Enjoy the ride!
 
+	// Instantiating engine, timer and simulator
+	const engine = new Jecs.Engine();
+	const util = new Util();
+
+	// Declare entities - this is like the model, but without data - we attach that later, as 'components'
+	function create_todoitem(title, completed, id) {
+		if (id == undefined)
+			id = util.uuid()
+		if (completed == undefined)
+			completed = false
+		let entity_name = `todoitem-${id}`  // entity names must be unique in jecs
+		let entity = engine.entity(entity_name)
+		entity.setComponent('data', {title,  completed, id})
+		return entity
+	}
+
+	// let todos = [] // do we actually want a collection - the ECS will do it for us!
+	create_todoitem("A")
+	create_todoitem("B", true)
+
+	// Systems
+
+	engine.system('controller-model', ['data'], (entity, { data }) => {
+		// if (entity.name == 'model-welcome-message') $('input[name=welcome]').val(data.val)
+		// else if (entity.name == 'model-firstname') $('input[name=firstname]').val(data.val)
+		// else if (entity.name == 'model-surname') $('input[name=surname]').val(data.val)
+		console.log(`controller-model: ${entity.name}, ${JSON.stringify(data)}`);
+	});
+
+	// Boot
+
+	engine.tick()
+
 })(window);
