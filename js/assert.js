a46bcf35b1e686d38999e9d66ae8a39f97d04c3e
Andy Bulka <abulka@gmail.com>
2020-04-25 17:00:17 +1000

    node module for jecs

---
+++ js/assert.js
@@ -0,0 +1,12 @@
+// Util
+
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
+
